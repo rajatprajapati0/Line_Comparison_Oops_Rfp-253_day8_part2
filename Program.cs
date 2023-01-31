a950300493828ae3d203bb06beef4dfe914d3bee
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-01 00:57:38 +0530

    Added: Line_comparison program file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Welcome to Line\r\nComparison Computation **** ");
+
+        }
+    }
+}
