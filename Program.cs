0f785bda5cb8a0530bd562679a206fd43eebb183
rajatprajapati0 <rajatprajapati09@gmail.com>
2023-02-01 22:56:05 +0530

    Added:UC4  comapare line length by  compareTo method

--- Program.cs
+++ Program.cs
@@ -3,80 +3,83 @@ using System.Buffers.Text;
 
 namespace Line_Comparison
 {
-
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("**** Welcome to Line\r\nComparison Computation **** ");
-
-            check();
-
-
+            Logic obj = new Logic();
+            obj.check();
         }
+       
+    }
 
 
-        static void check()
+
+    class Logic
+    {
+      public void check()
         {
             double lenght1 = 0;
             double lenght2 = 0;
 
-                for (int i = 1; i <= 2; i++)
+            for (int i = 1; i <= 2; i++)
+            {
+                if (i == 1)
                 {
-                  if (i == 1)
-                  {
                     Console.WriteLine("####### enter valuse for length of first line ######## ");
                     double length = read();
                     Console.WriteLine("Length of line**** " + length);
-                    lenght1= length;
-                  }
-                   else
-                  {
-                    Console.WriteLine("####### enter valuse for length of second line ######## ");
-                    double length= read();
-                    Console.WriteLine("Length of line**** " + length);
-                    lenght2= length;
-
-                   }
+                    lenght1 = length;
                 }
-                    
+                else
+                {
+                    Console.WriteLine("####### enter valuse for length of second line ######## ");
+                    double length = read();
+                    Console.WriteLine("Length of line**** " + length);
+                    lenght2 = length;
 
-                   if (lenght1.Equals(lenght2))
-                   {
-                    Console.WriteLine("length of both lines are equle");
-            
-                   }
-                   
-                   else if (lenght1>(lenght2))
-                   {
-                     Console.WriteLine("first line is greater than second line");
-
-                   }
-                   else 
-                   {
-                     Console.WriteLine("second line is greater than first line");
-
+                }
             }
+            int l = lenght1.CompareTo(lenght2);
 
-
-        }
-            static double read()
+            if (l==0)
             {
-                Console.WriteLine("****enter first two number (x1, y1) co-ordinates for point one  ");
-                Console.WriteLine("enter x1");
-                double x1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("enter y2");
-                double y1 = double.Parse(Console.ReadLine());
-                Console.WriteLine("****enter first two number (x2, y2) co-ordinates for point two  ");
-                Console.WriteLine("enter x2");
-                double x2 = double.Parse(Console.ReadLine());
-                Console.WriteLine("enter y2");
-                double y2 = double.Parse(Console.ReadLine());
-                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                Console.WriteLine("length of both lines are equle");
 
-
-                return length;
             }
+
+            else if (l > 0)
+            {
+                Console.WriteLine("first line is greater than second line");
+
+            }
+            else
+            {
+                Console.WriteLine("second line is greater than first line");
+
+            }
+
+
+      }
+        static double read()
+        {
+            Console.WriteLine("****enter first two number (x1, y1) co-ordinates for point one  ");
+            Console.WriteLine("enter x1");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("****enter first two number (x2, y2) co-ordinates for point two  ");
+            Console.WriteLine("enter x2");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter y2");
+            double y2 = double.Parse(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+
+            return length;
+        }
+
     }
 }
 
